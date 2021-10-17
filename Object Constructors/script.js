fb6ce6fb3ff6f19cb85b87cc7cbebad8ec3ccb2c
Chaz Carothers <chazcarothers@chazs-mbp.attlocal.net>
2021-10-17 17:22:45 -0500

    added obj constructors

---
+++ Object Constructors/script.js
@@ -0,0 +1,14 @@
+function Person(name, height, age) {
+  this.name = name;
+  this.height = height;
+  this.age = age;
+  this.updateAge = function () {
+    return ++this.age;
+  };
+}
+
+let person01 = new Person('Stevie Wonder', "6'0", '71');
+let person02 = new Person('Dolly Parton', "5'0", '75');
+
+console.log(person01.updateAge());
+console.log(person02.updateAge());
