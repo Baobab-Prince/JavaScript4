e3d83fc9a1c26a506c482c5be0f3cbe6bb00c5ec
Chaz Carothers <chaz.carothers@gmail.com>
2021-10-28 10:45:51 -0500

    added polymorphism

--- OOP/polymorphism.js
+++ OOP/polymorphism.js
@@ -1,14 +1,19 @@
+/*To call the parent class method -- super.eats(); under eats function)*/
+
 class Animal {
   constructor(name) {
     this.name = name;
   }
   eats() {
-    console.log(this.name + 'eats food');
+    console.log(this.name + ' eats food');
   }
 }
 
-class Alligator extends Animal {
-  eats() {
-    console.log(this.name + '');
-  }
+class Panda extends Animal {
+  //   eats() {
+  //     console.log(this.name + ' eats bamboo');
+  //   }
 }
+
+let buttercup = new Panda('Buttercup');
+buttercup.eats();
